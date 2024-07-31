62429e61e41cd9bc75c967a1fca9bbfa7b9bda46
Mahi <professorfjord4@gmail.com>
2024-07-31 12:53:16 +0900

    エラー処理と微調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,63 +9,68 @@ class HomeController < ApplicationController
     end
     puts insta_accountname
     @timestamp = insta_latest_timestamp_check(insta_accountname)
-    database_latest_data = Post.order(updated_at: :desc).limit(1)
-    database_latest_post_id = 0
-    database_latest_data.each do |data|
-      database_latest_post_id = data.id
-    end
-    database_account_latest_data = Post.where(account_name: insta_accountname).order(updated_at: :desc).limit(1)
-    database_latest_post_datetime = DateTime.new(2000,1,1)
-    if database_account_latest_data.present?
-      database_account_latest_data.each do |data|
-        database_latest_post_datetime = data.post_datetime
-        puts(database_latest_post_datetime)
-      end
-    end
-    nowtime = Time.now.utc
-    if database_latest_post_datetime == @timestamp and (nowtime - 5.days) <= database_latest_post_datetime
-      puts("latest_post_is_updated")
-      account_latest_post = Post.where(account_name: insta_accountname).order(updated_at: :desc).limit(1)
-      @latest_post = Image.where(postid: account_latest_post[0].id).limit(1)
-      # @latest_post = Image.order(updated_at: :desc).limit(1)
+    if @timestamp == "error"
+      flash[:danger] = "ERROR: Instagramアカウント名に誤りがあります。"
+      redirect_to root_path
     else
-      latest_data = insta_images_get(insta_accountname)
-      permalink = latest_data["permalink"]
-      post_datetime = latest_data["timestamp"]
-      if latest_data.has_key?("children")
-        latest_images = latest_data["children"]["data"]
-        has_children = 1
-        database_latest_post_id += 1
-        post = Post.new(id: database_latest_post_id,permalink: permalink, post_datetime: post_datetime, has_children: has_children, account_name: insta_accountname)
-        post.save
-        images_num = latest_images.length
-        10.times do |i|
-          if i < images_num
-            instance_variable_set('@image' + (i + 1).to_s, latest_images[i]["media_url"])
-            instance_variable_set('@mediatype' + (i + 1).to_s, latest_images[i]["media_type"])
-          else
-            instance_variable_set('@image' + (i + 1).to_s, nil)
-            instance_variable_set('@mediatype' + (i + 1).to_s, nil)
-          end
-        end
-        image = Image.new(postid: database_latest_post_id,image1: @image1, image2: @image2, image3: @image3, image4: @image4, image5: @image5, image6: @image6, image7: @image7, image8: @image8, image9: @image9, image10: @image10, mediatype1: @mediatype1, mediatype2: @mediatype2, mediatype3: @mediatype3, mediatype4: @mediatype4, mediatype5: @mediatype5, mediatype6: @mediatype6, mediatype7: @mediatype7, mediatype8: @mediatype8, mediatype9: @mediatype9, mediatype10: @mediatype10)
-        image.save
-        @latest_post = Image.order(updated_at: :desc).limit(1)
-        @latest_post
-      else
-        has_children = 0
-        database_latest_post_id += 1
-        post = Post.new(id: database_latest_post_id,permalink: permalink, post_datetime: post_datetime, has_children: has_children, account_name: insta_accountname)
-        post.save
-        latest_image_not_children = latest_data["media_url"]
-        latest_mediatype_not_children = latest_data["media_type"]
-        image = Image.new(postid: database_latest_post_id,image1: latest_image_not_children, mediatype1:latest_mediatype_not_children)
-        image.save
-        @latest_post = Image.order(updated_at: :desc).limit(1)
-        @latest_post
+      database_latest_data = Post.order(updated_at: :desc).limit(1)
+      database_latest_post_id = 0
+      database_latest_data.each do |data|
+        database_latest_post_id = data.id
       end
+      database_account_latest_data = Post.where(account_name: insta_accountname).order(updated_at: :desc).limit(1)
+      database_latest_post_datetime = DateTime.new(2000,1,1)
+      if database_account_latest_data.present?
+        database_account_latest_data.each do |data|
+          database_latest_post_datetime = data.post_datetime
+          puts(database_latest_post_datetime)
+        end
+      end
+      nowtime = Time.now.utc
+      if database_latest_post_datetime == @timestamp and (nowtime - 5.days) <= database_latest_post_datetime
+        puts("latest_post_is_updated")
+        account_latest_post = Post.where(account_name: insta_accountname).order(updated_at: :desc).limit(1)
+        @latest_post = Image.where(postid: account_latest_post[0].id).limit(1)
+        # @latest_post = Image.order(updated_at: :desc).limit(1)
+      else
+        latest_data = insta_images_get(insta_accountname)
+        permalink = latest_data["permalink"]
+        post_datetime = latest_data["timestamp"]
+        if latest_data.has_key?("children")
+          latest_images = latest_data["children"]["data"]
+          has_children = 1
+          database_latest_post_id += 1
+          post = Post.new(id: database_latest_post_id,permalink: permalink, post_datetime: post_datetime, has_children: has_children, account_name: insta_accountname)
+          post.save
+          images_num = latest_images.length
+          10.times do |i|
+            if i < images_num
+              instance_variable_set('@image' + (i + 1).to_s, latest_images[i]["media_url"])
+              instance_variable_set('@mediatype' + (i + 1).to_s, latest_images[i]["media_type"])
+            else
+              instance_variable_set('@image' + (i + 1).to_s, nil)
+              instance_variable_set('@mediatype' + (i + 1).to_s, nil)
+            end
+          end
+          image = Image.new(postid: database_latest_post_id,image1: @image1, image2: @image2, image3: @image3, image4: @image4, image5: @image5, image6: @image6, image7: @image7, image8: @image8, image9: @image9, image10: @image10, mediatype1: @mediatype1, mediatype2: @mediatype2, mediatype3: @mediatype3, mediatype4: @mediatype4, mediatype5: @mediatype5, mediatype6: @mediatype6, mediatype7: @mediatype7, mediatype8: @mediatype8, mediatype9: @mediatype9, mediatype10: @mediatype10)
+          image.save
+          @latest_post = Image.order(updated_at: :desc).limit(1)
+          @latest_post
+        else
+          has_children = 0
+          database_latest_post_id += 1
+          post = Post.new(id: database_latest_post_id,permalink: permalink, post_datetime: post_datetime, has_children: has_children, account_name: insta_accountname)
+          post.save
+          latest_image_not_children = latest_data["media_url"]
+          latest_mediatype_not_children = latest_data["media_type"]
+          image = Image.new(postid: database_latest_post_id,image1: latest_image_not_children, mediatype1:latest_mediatype_not_children)
+          image.save
+          @latest_post = Image.order(updated_at: :desc).limit(1)
+          @latest_post
+        end
+      end
+      # @insta_img = insta_images_get("yomiuri.giants")
     end
-    # @insta_img = insta_images_get("yomiuri.giants")
   end
 
   def insta_latest_timestamp_check(account_name)
@@ -81,11 +86,13 @@ class HomeController < ApplicationController
 
     json_response_body = JSON.parse(response.body)
 
-
-    recent_timestamp_str = json_response_body["business_discovery"]["media"]["data"][0]["timestamp"]
-    recent_timestamp = Time.parse(recent_timestamp_str)
-
-    return recent_timestamp
+    if json_response_body.has_key?("error")
+      return "error"
+    else
+      recent_timestamp_str = json_response_body["business_discovery"]["media"]["data"][0]["timestamp"]
+      recent_timestamp = Time.parse(recent_timestamp_str)
+      return recent_timestamp
+    end
   end
 
   def insta_images_get(account_name)
@@ -105,10 +112,5 @@ class HomeController < ApplicationController
     recent_data = json_response_body["business_discovery"]['media']["data"][0]
     recent_data
   end
-
-  # def account_change
-  #   account_name = params[:account_name]
-  #   top(account_name)
-  # end
 end
 
